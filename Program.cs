92bc020fbef59c5522141decbd185bf50a693446
YovchoKalev <yovchokalev@gmail.com>
2017-04-08 21:38:11 +0300

    upload

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿namespace besenica
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var playWords = new List<string>();
+            playWords.Add("ibrahim");
+            playWords.Add("yovcho");
+			playWords.Add("banan");
+			playWords.Add("ventilator");
+
+            string gameWord = playWords[new Random().Next(0, playWords.Count)];
+
+            var listOfChars = gameWord.ToCharArray();
+            var listOfWrongLettersGuessed = new List<char>();
+            int countOfFails = 0;
+            Dictionary<char, List<int>> guessedLetters = new Dictionary<char, List<int>>();
+
+            Console.WriteLine("Enter a letter a-z");
+            while (true)
+            {
+                PrintGuessedLetters(guessedLetters);
+                PrintEmptyDashes(gameWord);
+                Hangman.PrintHangman(countOfFails);
+                char pressed = (Console.ReadKey().KeyChar);
+
+                if (pressed >= 'a' && pressed <= 'z')
+                {
+                    if (listOfChars.Contains(pressed))
+                    {
+                        List<int> addingLetters = new List<int>();
+
+                        for (int i = 0; i < listOfChars.Length; i++)
+                        {
+                            if (listOfChars[i] == pressed)
+                            {
+                                addingLetters.Add(i);
+                            }
+                        }
+                        try
+                        {
+                            guessedLetters.Add(pressed, addingLetters);
+                        }
+                        catch (ArgumentException)
+                        {
+                            Console.WriteLine("Veche q napisahme tazi bukva ve shtarkel");
+                        }
+                        
+                    }
+                    else if (listOfWrongLettersGuessed.Contains(pressed))
+                    {
+                        Console.WriteLine("Veche q probva taq bukva i ne stana, baven li si?");
+                    }
+                    else
+                    {
+                        listOfWrongLettersGuessed.Add(pressed);
+                        countOfFails++;
+                        if (countOfFails > 8)
+                        {
+                            Console.WriteLine("you lost the game");
+                            return;
+                        }
+                    }
+                }
+                
+            }
+        }
+        static void PrintEmptyDashes(string word)
+        {
+            Console.SetCursorPosition(53, 25);
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write("_ ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintGuessedLetters(Dictionary<char, List<int>> guessedLetters)
+        {
+            
+            int x = 53;
+            foreach (var item in guessedLetters)
+            {
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    Console.SetCursorPosition(item.Value[i]*2 + x, 24);
+                    Console.Write(item.Key);
+                }
+            }
+        }
+    }
+}
---
+++ hangman.cs
@@ -0,0 +1,175 @@
+﻿namespace besenica
+{
+    using System;
+
+    public class Hangman
+    {
+        public static void PrintHangman(int count)
+        {
+            if (count == 1)
+            {
+                FirstFail();
+            }
+            else if (count == 2)
+            {
+                FirstFail();
+                SecondFail();
+            }
+            else if (count == 3)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+            }
+            else if (count == 4)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+                FourthFail();
+            }
+            else if (count == 5)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+                FourthFail();
+                FifthFail();
+            }
+            else if (count == 6)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+                FourthFail();
+                FifthFail();
+                SixthFail();
+            }
+            else if (count == 7)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+                FourthFail();
+                FifthFail();
+                SixthFail();
+                SeventhFail();
+            }
+            else if (count == 8)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+                FourthFail();
+                FifthFail();
+                SixthFail();
+                SeventhFail();
+                EightFail();
+            }
+            else if (count == 9)
+            {
+                FirstFail();
+                SecondFail();
+                ThirdFail();
+                FourthFail();
+                FifthFail();
+                SixthFail();
+                SeventhFail();
+                EightFail();
+                NinthFail();
+            }
+        }
+        private static void FirstFail()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.SetCursorPosition(50, i);
+                Console.WriteLine('*');
+            }
+        }
+        private static void SecondFail()
+        {
+            Console.SetCursorPosition(50, 0);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+        private static void ThirdFail()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(58, i);
+                Console.WriteLine('*');
+            }
+        }
+        private static void FourthFail()
+        {
+            Console.SetCursorPosition(56, 4);
+            Console.WriteLine("*****");
+            Console.SetCursorPosition(56, 5);
+            Console.WriteLine("*   *");
+            Console.SetCursorPosition(56, 6);
+            Console.WriteLine("*   *");
+            Console.SetCursorPosition(56, 7);
+            Console.WriteLine("*****");
+        }
+        private static void FifthFail()
+        {
+            for (int i = 8; i < 16; i++)
+            {
+                Console.SetCursorPosition(58, i);
+                Console.WriteLine('*');
+            }
+        }
+        private static void SixthFail()
+        {
+            int x = 55;
+            int y = 8;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine('*');
+                y++;
+                x--;
+            }
+        }
+        private static void SeventhFail()
+        {
+            int x = 61;
+            int y = 8;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine('*');
+                y++;
+                x++;
+            }
+        }
+        private static void EightFail()
+        {
+            int x = 55;
+            int y = 16;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine('*');
+                y++;
+                x--;
+            }
+        }
+        private static void NinthFail()
+        {
+            int x = 61;
+            int y = 16;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine('*');
+                y++;
+                x++;
+            }
+        }
+    }
+}
